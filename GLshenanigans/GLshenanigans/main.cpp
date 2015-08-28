8b7f5a5e87f1b2ba6d43e734946e77fb4e73150b
Quinn Mosher <quinn.jmosher@gmail.com>
2015-08-28 13:37:06 -0700

    draws a grid

--- GLshenanigans/GLshenanigans/main.cpp
+++ GLshenanigans/GLshenanigans/main.cpp
@@ -23,9 +23,9 @@ const int set_window_width = 1280;
 const int set_window_height = 720;
 const char* set_window_name = "I am a Window";
 //clear color
-const float set_clearScr_r = 0.25f;
-const float set_clearScr_g = 0.25f;
-const float set_clearScr_b = 0.25f;
+const float set_clearScr_r = 0.35f;
+const float set_clearScr_g = 0.35f;
+const float set_clearScr_b = 0.35f;
 const float set_clearScr_a = 1;
 
 //running vars
@@ -51,7 +51,7 @@ int Startup() {
 	}
 
 	//create window
-	window = glfwCreateWindow(set_window_width, set_window_height, set_window_name, nullptr, nullptr);
+	/*glfwWindow**/ window = glfwCreateWindow(set_window_width, set_window_height, set_window_name, nullptr, nullptr);
 	if (window == nullptr) {
 		glfwTerminate();
 		return -2;
@@ -68,8 +68,8 @@ int Startup() {
 	//ready Gizmos
 	Gizmos::create();
 
-	view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
-	projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+	/*mat4*/ view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	/*mat4*/ projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
 
 	//settings
 	glClearColor(set_clearScr_r, set_clearScr_g, set_clearScr_b, set_clearScr_a);
@@ -94,6 +94,25 @@ void Run() {
 	while (glfwWindowShouldClose(window) == false) {
 		//clear buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		Gizmos::clear();
+
+		//calculate
+		Gizmos::addTransform(glm::mat4(1));
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 21; i++) {
+			Gizmos::addLine(vec3(-10 + i, 0, 10),
+							vec3(-10 + i, 0, -10),
+							i == 10 ? white : black);
+			Gizmos::addLine(vec3(10, 0, -10 + i),
+							vec3(-10, 0, -10 + i),
+							i == 10 ? white : black);
+		}
+
+		//draw
+		Gizmos::draw(projection * view);
 
 		//glfw update
 		glfwSwapBuffers(window);
