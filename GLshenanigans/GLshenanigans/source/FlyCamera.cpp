ee829703ef762b927ab394eec6dce4b41ada5a16
Quinn Mosher <quinn.jmosher@gmail.com>
2015-09-03 16:49:00 -0700

    removed redundant multiplication

--- GLshenanigans/GLshenanigans/source/FlyCamera.cpp
+++ GLshenanigans/GLshenanigans/source/FlyCamera.cpp
@@ -53,7 +53,7 @@ void FlyCamera::update(float in_deltaTime) {
 	vec3 turnRightLeft = glm::rotate(forward, (rotateSensitivity * in_deltaTime) * -deltaMouse.y, right);
 	vec3 turnUpDown = glm::rotate(forward, (rotateSensitivity * in_deltaTime) * -deltaMouse.x, up);
 	//turn camera
-	this->setLookAt(currentPos, inFrontOfMe + (turnRightLeft + turnUpDown) * 10, up);
+	this->setLookAt(currentPos, inFrontOfMe + (turnRightLeft + turnUpDown), up);
 	
 }
 void FlyCamera::setSpeed(float in_speed) {
