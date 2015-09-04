5124ebd0342d2726b17e8936dc20ae97a46a80ae
Quinn Mosher <quinn.jmosher@gmail.com>
2015-09-04 10:31:45 -0700

    working on input handler

---
+++ GLshenanigans/GLshenanigans/source/Input.cpp
@@ -0,0 +1,81 @@
+#include <Input.h>
+#include <GLFW\glfw3.h>
+
+void Input::Init(GLFWwindow* in_context) {
+	context = in_context;
+}
+
+void Input::Update() {
+
+	//take and handle keystates for keys
+	for (int i = 0; i < KeyCode::KEY_LAST; i++) {
+		//get keyState
+		bool isCurrentDown = (glfwGetKey(context, i) == GLFW_PRESS);
+		if (isCurrentDown) {
+			//if the key IS down but wasent on the last frame 
+			if (curKeyStates[i] == KeyState::Up || curKeyStates[i] == KeyState::Released) {
+				curKeyStates[i] = KeyState::Pressed;//then it has just been pressed
+			}//if the key was pressed last frame and still is down then it is now "Down"
+			else if (curKeyStates[i] == KeyState::Pressed) {
+				curKeyStates[i] = KeyState::Down;
+			}
+		}
+		else
+		{
+			//if the key isnt down ad it was last frame
+			if (curKeyStates[i] == KeyState::Down || curKeyStates[i] == KeyState::Pressed) {
+				curKeyStates[i] = KeyState::Released;//then it has just been released
+			}//if the key was realeased last frame an is still up then ut us now "Up"
+			else if (curKeyStates[i] == KeyState::Released) {
+				curKeyStates[i] = KeyState::Up;
+			}
+		}
+	}
+
+	//take and handle keystates for buttons
+	for (int i = 0; i < MouseButtonCode::MOUSE_BUTTON_LAST; i++) {
+		bool isCurrentDown = (glfwGetKey(context, i) == GLFW_PRESS);
+		if (isCurrentDown) {
+			if (curMouseStates[i] == KeyState::Up || curMouseStates[i] == KeyState::Released) {
+				curMouseStates[i] = KeyState::Pressed;
+			}
+			else if (curMouseStates[i] == KeyState::Pressed) {
+				curMouseStates[i] = KeyState::Down;
+			}
+		}
+		else
+		{
+			if (curMouseStates[i] == KeyState::Down || curMouseStates[i] == KeyState::Pressed) {
+				curMouseStates[i] = KeyState::Released;
+			}
+			else if (curMouseStates[i] == KeyState::Released) {
+				curMouseStates[i] = KeyState::Up;
+			}
+		}
+	}
+
+	//handle mouse position
+	lastMousePos = vec2(curMousePos);
+	double curMouseX, curMouseY;
+	glfwGetCursorPos(context, &curMouseX, &curMouseY);
+	curMousePos.x = curMouseX;
+	curMousePos.y = curMouseY;
+	vec2 deltaMouse = vec2(curMouseX - lastMousePos.x, curMouseY - lastMousePos.y);
+}
+
+//key stuff
+
+KeyState Input::GetKey(KeyCode in_key) {
+	return curKeyStates[in_key];
+}
+
+bool Input::IsKey(KeyCode in_key, KeyState in_state) {
+
+}
+
+//mouse stuff
+
+KeyState Input::GetMouseButton(MouseButtonCode in_button);
+bool Input::IsMouseButton(MouseButtonCode in_button, KeyState in_state);
+vec2 Input::GetMousePos();
+vec2 Input::GetMouseDelta();
