7130bff516cdc1c34bd0661f7fbdaff919116fa9
Quinn Mosher <quinn.jmosher@gmail.com>
2015-10-02 15:09:37 -0700

    fully woking, playing with shader

--- GLshenanigans/GLshenanigans/assets/shaders/postFrag.glsl
+++ GLshenanigans/GLshenanigans/assets/shaders/postFrag.glsl
@@ -9,10 +9,78 @@ vec4 Simple() {
 	return texture(diffuse,vTexCoord);
 }
 
+vec4 BoxBlur() {
+	vec2 texel = 1.0f / textureSize(diffuse, 0).xy;
+	
+	vec4 color = texture(diffuse, vTexCoord);
+	color += texture(diffuse, vTexCoord + vec2(-texel.x,  texel.y));
+	color += texture(diffuse, vTexCoord + vec2(-texel.x,   	    0));
+	color += texture(diffuse, vTexCoord + vec2(-texel.x, -texel.y));
+	color += texture(diffuse, vTexCoord + vec2(		  0,  texel.y));
+	color += texture(diffuse, vTexCoord + vec2(		  0, -texel.y));
+	color += texture(diffuse, vTexCoord + vec2( texel.x,  texel.y));
+	color += texture(diffuse, vTexCoord + vec2( texel.x, 		0));
+	color += texture(diffuse, vTexCoord + vec2( texel.x, -texel.y));
+	
+	return color / 9;
+}
 
+vec4 Distort() {
+	vec2 mid = vec2(0.5f);
+	
+	float distanceFromCentre = distance(vTexCoord, mid);
+	vec2 normalizedCoord = normalize(vTexCoord - mid);
+	float bias = distanceFromCentre + sin(distanceFromCentre * 15) * 0.02f;
+	
+	vec2 newCoord = mid + bias * normalizedCoord;
+	return texture(diffuse, newCoord);
+}
+
+vec4 GrayScale() {
+	vec4 basecolor = texture(diffuse, vTexCoord);
+	float avgColor = (basecolor.x + basecolor.y + basecolor.z) / 3;
+	return vec4(avgColor, avgColor, avgColor, 1);
+}
+
+vec4 ScanLines() {
+	vec2 texel = 1.0f / textureSize(diffuse, 0).xy;
+	
+	vec4 color = texture(diffuse, vTexCoord);
+	
+	int yPos = int(textureSize(diffuse, 0).y * vTexCoord.y);
+	if (yPos % 3 == 0) {
+		color = (color + vec4(0, 0.5f, 0, 1)) / 2;
+	}
+	
+	return color;
+	
+}
+
+vec4 Sharpen() {
+	vec2 texel = 1.0f / textureSize(diffuse, 0);
+	
+	vec4 color = texture(diffuse, vTexCoord) * (11.0f / 3.0f);
+	color += texture(diffuse, vTexCoord + vec2(0, texel.y)) * (-2.0f / 3.0f);
+	color += texture(diffuse, vTexCoord - vec2(0, texel.y)) * (-2.0f / 3.0f);
+	color += texture(diffuse, vTexCoord + vec2(texel.x, 0)) * (-2.0f / 3.0f);
+	color += texture(diffuse, vTexCoord - vec2(texel.x, 0)) * (-2.0f / 3.0f);
+	color.w = 1.0f;
+	
+	return color;
+}
+
+/*vec4 Sobel() {
+	vec4 colorX = vec4(0, 0, 0, 1);
+	vec4 colorY = vec4(0, 0, 0, 1);
+	
+	vec2 texel = 1.0f / textureSize(diffuse, 0);
+	
+	//x
+	colorX 
+}*/
 
 void main() {
 
-	FragColor = Simple();
+	FragColor = Sharpen();
 	
 }
